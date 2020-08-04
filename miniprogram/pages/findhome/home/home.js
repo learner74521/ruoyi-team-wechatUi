ab4fc3639249166a1dfd1a5df176336e0deeb8c1
learner74521 <47235522+learner74521@users.noreply.github.com>
2020-08-04 17:45:29 +0800

    ruoyi-teamUI前端

--- miniprogram/pages/findhome/home/home.js
+++ miniprogram/pages/findhome/home/home.js
@@ -1,4 +1,5 @@
 // pages/findhome/find/find.js
+const app=getApp();
 Component({
   options: {
     addGlobalClass: true,
@@ -7,7 +8,14 @@ Component({
    * 页面的初始数据
    */
   data: {
-
+    TabCur: 0,
+    scrollLeft:0,
+    number:0,
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    Custom: app.globalData.Custom,
+    bgTopUrl:"https://7778-wx-teamyml-2020-1301686336.tcb.qcloud.la/bgImageUrl-2020/bgFind.jpg?sign=074d3b2cef543697aaf9101c96636c72&t=1594974053",
+    tabSelect:["推荐","招募广场","附近",]
   },
   lifetimes: {
     // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
@@ -20,6 +28,22 @@ Component({
     show: function () {console.log("show") },
     hide: function () {console.log("hide") },
     resize: function () { console.log("resize")},
+  },
+  methods:{
+    tabSelect(e) {
+      this.setData({
+        TabCur: e.currentTarget.dataset.id,
+        scrollLeft: (e.currentTarget.dataset.id-1)*60
+      })
+    },
+    onScrollChange(e){
+    var scrollTop=parseInt(e.detail.scrollTop)
+     if(scrollTop<=300){
+         this.setData({
+          number:scrollTop/300
+         })
+     }
+     var num=this.data.number
+    }
   }
-  
 })
